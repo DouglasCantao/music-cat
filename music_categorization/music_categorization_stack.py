0aa7ded326ff7fe4042ad135880745b74711a8d2
Douglas Cantao <dcp.pereira@gmail.com>
2026-01-08 16:21:12 -0800

    Stack improvments
    Simplifying the IAM role to allow the lambda function construct create the role automatically and then use the source_bucket.grant_read() method.
    Removing unused code and adding some comments.

--- music_categorization/music_categorization_stack.py
+++ music_categorization/music_categorization_stack.py
@@ -4,7 +4,6 @@ from aws_cdk import (
     aws_lambda as _lambda,
     aws_events as events,
     aws_events_targets as targets,
-    aws_iam as iam,
     RemovalPolicy
 )
 from constructs import Construct
@@ -14,58 +13,31 @@ class MusicCategorizationStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # 1. Create the S3 Bucket
-        # We must enable 'event_bridge_enabled' for S3 to send events to the bus
+        # 1. Create the S3 Bucket with EventBridge enabled
         source_bucket = s3.Bucket(
             self, "IncomingFilesBucket",
             versioned=True,
-            removal_policy=RemovalPolicy.DESTROY, # Only for dev/testing
-            auto_delete_objects=False,            # Only for dev/testing
-            event_bridge_enabled=True            # Critical step!
+            removal_policy=RemovalPolicy.DESTROY,
+            auto_delete_objects=True,  # Automatically delete objects on `cdk destroy`
+            event_bridge_enabled=True
         )
 
-        # 2a. Create an IAM Role for the Lambda
-        # lambda_role = iam.Role(
-        #     self, "FileProcessorRole",
-        #     assumed_by=iam.ServicePrincipal("lambda.amazonaws.com")
-        # )
-        # lambda_role.add_managed_policy(
-        #     iam.ManagedPolicy.from_aws_managed_policy_name("service-role/AWSLambdaBasicExecutionRole")
-        # )
-
-        # 1. Define the IAM Role
-        lambda_role = iam.Role(self, "LambdaS3ExecutionRole",
-            # Trust policy: allows the Lambda service to use this role
-            assumed_by=iam.ServicePrincipal("lambda.amazonaws.com"),
-            # Add basic logging permissions (recommended)
-            managed_policies=[
-                iam.ManagedPolicy.from_aws_managed_policy_name("service-role/AWSLambdaBasicExecutionRole")
-            ]
-        )
-
-        # 2. Add an inline policy to the role for S3 access
-        lambda_role.add_to_policy(iam.PolicyStatement(
-            effect=iam.Effect.ALLOW,
-            actions=["s3:GetObject", "s3:ListBucket"],
-            resources=[
-                source_bucket.bucket_arn,          # Permission for the bucket itself
-            ]
-        ))
-
-        # 2b. Create the Lambda Function
+        # 2. Create the Lambda Function
+        # The IAM role will be automatically created by the CDK with the necessary
+        # basic execution permissions.
         processor_lambda = _lambda.Function(
             self, "FileProcessorHandler",
             runtime=_lambda.Runtime.PYTHON_3_12,
             handler="file_handler.s3_file_handler",
             code=_lambda.Code.from_asset("lambda"),
-            role=lambda_role  # Assign the role here
         )
 
-        # 3. Grant the Lambda permission to read from the bucket
+        # 3. Grant the Lambda permission to read from the S3 bucket
+        # This is the idiomatic CDK way to grant permissions. It adds the
+        # necessary policies to the Lambda's execution role.
         source_bucket.grant_read(processor_lambda)
 
-        # 4. Create an EventBridge Rule
-        # This rule listens for "Object Created" events from our specific bucket
+        # 4. Create an EventBridge Rule to trigger the Lambda
         s3_event_rule = events.Rule(
             self, "S3UploadRule",
             event_pattern=events.EventPattern(
@@ -79,5 +51,5 @@ class MusicCategorizationStack(Stack):
             )
         )
 
-        # 5. Add the Lambda as the target for the EventBridge Rule
+        # 5. Set the Lambda function as the target for the EventBridge rule
         s3_event_rule.add_target(targets.LambdaFunction(processor_lambda))
